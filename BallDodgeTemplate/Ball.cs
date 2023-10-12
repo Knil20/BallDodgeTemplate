39fbeb881e6c079d29c732771e546eaab3d8e6a2
Tony Theodoropoulos <theodot@outlook.com>
2023-10-12 12:24:31 -0400

    Add project files.

---
+++ BallDodgeTemplate/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallDodgeTemplate
+{
+    internal class Ball
+    {
+        public int size = 10;
+        public int x, y, xSpeed, ySpeed;
+
+        public Ball(int _x, int _y, int _xSpeed, int _ySpeed)
+        {
+            x = _x;
+            y = _y;
+            xSpeed = _xSpeed;
+            ySpeed = _ySpeed;
+        }
+
+        public void Move(string direction)
+        {
+            if (direction == "left")
+            {
+                x -= xSpeed;
+            }
+            else if (direction == "right")
+            {
+                x += xSpeed;
+            }
+
+            if (direction == "up")
+            {
+                y -= ySpeed;
+            }
+            else if (direction == "down")
+            {
+                y += ySpeed;
+            }
+        }
+
+        public void Move()
+        {
+            x += xSpeed;
+            y += ySpeed;
+
+            if (x < 0 || x > 600 - size)
+            {
+                xSpeed *= -1;
+            }
+
+            if (y < 0 || y > 500 - size)
+            {
+                ySpeed *= -1;
+            }
+        }
+    }
+}
---
+++ BallDodgeTemplate/GameScreen.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallDodgeTemplate
+{
+    public partial class GameScreen : UserControl
+    {
+        Ball hero = new Ball(100, 100, 10, 10);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Boolean leftArrowDown, rightArrowDown, upArrowDown, downArrowDown;
+
+        List<Ball> balls = new List<Ball>();
+
+        public GameScreen()
+        {
+            InitializeComponent();
+            Ball enemyBall = new Ball(150, 100, 10, 10);
+            balls.Add(enemyBall);
+
+            enemyBall = new Ball(250, 250, -10, 10);
+            balls.Add(enemyBall);
+
+            enemyBall = new Ball(400, 150, 10, -10);
+            balls.Add(enemyBall);
+
+        }
+
+        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+
+        private void GameScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move hero
+            if(leftArrowDown && hero.x > 0)
+            {
+                hero.Move("left");
+            }
+            else if (rightArrowDown && hero.x < this.Width - hero.size)
+            {
+                hero.Move("right");
+            }
+
+            if (upArrowDown && hero.y > 0)
+            {
+                hero.Move("up");
+            }
+            else if(downArrowDown && hero.y < this.Height - hero.size)
+            {
+                hero.Move("down");
+            }
+
+            //move enemies
+            foreach(Ball b in balls)
+            {
+                b.Move();
+            }
+         
+
+            Refresh();
+        }
+
+        private void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(whiteBrush, hero.x, hero.y, hero.size, hero.size);
+
+            foreach (Ball b in balls)
+            {
+                e.Graphics.FillEllipse(whiteBrush, b.x, b.y, b.size, b.size);
+            }
+        }
+    }
+}
